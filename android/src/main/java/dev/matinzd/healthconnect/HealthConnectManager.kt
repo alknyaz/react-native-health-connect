6fecb44ab89706c4ef59592898746049f38f2064
Alex Knyazkov <alex@knyazkov.com>
2023-09-06 11:38:14 -0400

    fix(android): fix imports

--- android/src/main/java/dev/matinzd/healthconnect/HealthConnectManager.kt
+++ android/src/main/java/dev/matinzd/healthconnect/HealthConnectManager.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import androidx.health.connect.client.HealthConnectClient
+import androidx.health.connect.client.request.ChangesTokenRequest
 import com.facebook.react.bridge.ActivityEventListener
 import com.facebook.react.bridge.Promise
 import com.facebook.react.bridge.ReactApplicationContext
